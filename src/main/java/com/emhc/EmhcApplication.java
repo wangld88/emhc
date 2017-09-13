c3c3b417fd08a6f42bb6f423e5972b2bd4c76cba
dong <ldcti@hotmail.com>
2017-09-12 22:31:55 -0400

    Init version

---
+++ src/main/java/com/emhc/EmhcApplication.java
@@ -0,0 +1,12 @@
+package com.emhc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmhcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmhcApplication.class, args);
+	}
+}
