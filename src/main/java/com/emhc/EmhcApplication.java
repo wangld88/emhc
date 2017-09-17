beaf27ff471c8a65740f608565c8a5c8116a5397
Jerry <wangld88@gmail.com>
2017-09-17 10:04:10 -0400

    New UI template

--- src/main/java/com/emhc/EmhcApplication.java
+++ src/main/java/com/emhc/EmhcApplication.java
@@ -1,11 +1,24 @@
 package com.emhc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
-public class EmhcApplication {
+/*@Configuration
+@ComponentScan
+@EnableAutoConfiguration*/
+public class EmhcApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmhcApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(EmhcApplication.class, args);
 	}
