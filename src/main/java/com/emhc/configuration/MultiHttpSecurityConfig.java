87bcca715824143c33951c8055f707c33cca0d1b
dong <ldcti@hotmail.com>
2017-09-17 14:08:06 -0400

    pom.xml remove 2018
    remove @enableWebsecurity in MultiHttpSecurityConfig.java

--- src/main/java/com/emhc/configuration/MultiHttpSecurityConfig.java
+++ src/main/java/com/emhc/configuration/MultiHttpSecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
 public class MultiHttpSecurityConfig {
 
 	@Autowired
