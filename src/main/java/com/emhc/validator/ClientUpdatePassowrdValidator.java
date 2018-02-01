f46364cd387040564462299885badc7d2e2e2ab9
dong <ldcti@hotmail.com>
2018-02-01 11:41:54 +0800

    client updatepassword validator

--- src/main/java/com/emhc/validator/ClientUpdatePassowrdValidator.java
+++ src/main/java/com/emhc/validator/ClientUpdatePassowrdValidator.java
@@ -2,22 +2,35 @@ package com.emhc.validator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.emhc.dto.ClientUpdatePassword;
+import com.emhc.dto.UserForm;
+import com.emhc.model.User;
+import com.emhc.service.UserService;
+
 
 
 @Component
 public class ClientUpdatePassowrdValidator implements Validator {
 
     private static final Logger logger = LoggerFactory.getLogger(ClientUpdatePassowrdValidator.class);
-    
- 
-    
- 
+   
+    private final UserService userService;
+
+    @Autowired
+    public ClientUpdatePassowrdValidator(UserService UserService) {
+        this.userService = UserService;
+    }
+        
     @Override
     public boolean supports(Class<?> clazz) {
         return clazz.equals(ClientUpdatePassword.class);
@@ -36,6 +49,7 @@ public class ClientUpdatePassowrdValidator implements Validator {
     }
 
     
+     
     private void validatePasswords(Errors errors, ClientUpdatePassword form) {
         /*//If need to enter the old password
         final Student std = studentService.getStudentByNumber(SecurityContextHolder.getContext().getAuthentication().getName());
@@ -44,6 +58,20 @@ public class ClientUpdatePassowrdValidator implements Validator {
         }
         */
     	
+    	// old password match
+    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+     	String usrname = auth.getName();
+     	User usr  = userService.getByUsername(usrname);
+		String logpsw = usr.getPassword();
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String psw = form.getOldpassword();
+			 
+			
+    	    if (!passwordEncoder.matches(psw,logpsw)){
+    	 		logger.info("oldpsw.nomatch :" + psw + ", " + logpsw);
+    	  		errors.rejectValue("password","NotMatch.updatepassword.oldpasswordconfirm");
+    	}
+    	//password confirm match
         if (!form.getPassword().equals(form.getConfirmpassword())) {
         	logger.info("password.no_match :" + form.getPassword() + ", " + form.getConfirmpassword());
             errors.rejectValue("password", "NotMatch.resetPassword.newPasswordConfirm");
